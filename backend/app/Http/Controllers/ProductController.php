30c2c679cefae83c01ed6644f531eb8da40daf0e
Andualem <andualem.fereja12@gmail.com>
2025-01-14 18:55:30 +0300

    backend update: api end points created

---
+++ backend/app/Http/Controllers/ProductController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Category;
+
+class ProductController extends Controller implements HasMiddleware
+{
+    /**
+     * Get the middleware that should be assigned to the controller.
+     */
+    public static function middleware(): array
+    {
+        return  [new Middleware('auth', except: ['index','show'])];
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::with('category')->get();
+        return response()->json(['message' => 'welcome']);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required',
+            'price' => 'required',
+            'quantity' => 'required',
+            'image' => 'required',
+            'category' => 'required|string',
+        ]);
+        $path = $request->file('profilepic')->getClientOriginalName();
+        Storage::put("images/{$path}", file_get_contents($request->file('profilepic')->getRealPath()));
+
+        //$user = auth()->user();
+        $category = Category::create([
+            'name' => $request->category,
+        ]);
+
+        $product = Product::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'quantity' => $request->quantity,
+            'image_path' => $path,
+            'category_id' => $category->id,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $product = Product::with('category')->find($id);
+
+        return response()->json($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,5 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::get('/api', function () {
     return response()->json(['name'=>'andualem']);
 });
+
+Route::apiResource('products', ProductController::class);
